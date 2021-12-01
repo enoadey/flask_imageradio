f452263f8d8866d1d180741fb628b74956c7a35c
enoadey <saviourcrown@icloud.com>
2021-12-01 19:05:50 +0100

    init:flask app

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+function readURL(input) {
+    if (input.files) {
+      var file = document.getElementById("img").files.length;
+        console.log(file)
+        for (var i = 0; i < file; i++) {
+        var reader = new FileReader();
+        reader.onload = function (e) {    
+          var tr = document.createElement("tr")
+          var td = document.createElement("td")
+          var imag = document.createElement('img')
+          imag.src= e.target.result
+          var tbody = document.getElementById("tbimage")
+          td.append(imag)
+          tr.append(td)
+          tbody.append(tr)
+          var img = e.target.result
+        }
+        reader.readAsDataURL(input.files[i]);
+      }
+      
+    }
+  };
